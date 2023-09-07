cc15bdc85a6d1b8c0cc5ebfd66ffc9b7c28c6ec3
vfreis09 <reis.vicente27@gmail.com>
2023-09-07 15:01:39 -0300

    custom components part 1

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,16 @@ import ReactDOM from "react-dom/client";
 //import App from "./App.jsx";
 import "./index.css";
 
-const page = (
-  <div>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first release in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
+function Page() {
+  return (
+    <div>
+      <ol>
+        <li>React is a hireable skill</li>
+        <li>I like coding frontend</li>
+        <li>I want to become a full stack dev</li>
+      </ol>
+    </div>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById("root")).render(page);
+ReactDOM.createRoot(document.getElementById("root")).render(<Page />);
