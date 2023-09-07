e69d7e6dfc421621a77f327bc2aa9197c44a7142
vfreis09 <reis.vicente27@gmail.com>
2023-09-07 14:27:48 -0300

    Project part 1 - markup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom/client";
 //import App from "./App.jsx";
 import "./index.css";
 
-const page = <h1>hello world</h1>;
+const page = (
+  <div>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first release in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(page);
