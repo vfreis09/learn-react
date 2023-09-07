f574fde816c27e7458c546cc27c22344d721e36e
vfreis09 <reis.vicente27@gmail.com>
2023-09-07 15:22:03 -0300

    custom components part 2

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,19 @@ import "./index.css";
 function Page() {
   return (
     <div>
+      <header>
+        <nav>
+          <img src="./react-logo.png" width="40px"></img>
+        </nav>
+      </header>
+      <h1>Reason I'm excited to learn React</h1>
       <ol>
-        <li>React is a hireable skill</li>
-        <li>I like coding frontend</li>
-        <li>I want to become a full stack dev</li>
+        <li>
+          It's a popular library, so I'll be able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
       </ol>
+      <footer>© 2023 Fernandes development. All rights reserved.</footer>
     </div>
   );
 }
